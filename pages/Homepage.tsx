0c55b882f00071a727c551dbadbe22d049d73400
karensin <kkwsin@gmail.com>
2022-06-04 11:46:07 -0700

    carousel fix

--- pages/Homepage.tsx
+++ pages/Homepage.tsx
@@ -12,11 +12,14 @@ declare module "*.jpg";
 
 
 const carouselConfig = (imgString: string, startNum: number, endNum: number) => {
+
     let imgList = []
 
-    for (let i: number = startNum; i < endNum; i++) {
+    for (let i: number = startNum; i <= endNum; i++) {
         imgList.push(`./caroselImages/${imgString}(${i}).jpg`)
     }
+    console.log(imgList)
+
     return imgList
 }
 export const productsMock = [
@@ -54,7 +57,7 @@ export const productsMock = [
         price: 'USD $12,850',
         prodSpecs: 'CASE: Very thick  and sharp case that shows light wear from use. Excellent black bezel insert also in good condition and show light wear from use. / DIAL: Very good WG surround dial and matching hands. The glossy surface of the  dial is ageing to matte and has some spotting from age BRACELET: Excellent 93150 bracelet with 10 links showing very light stretch for its age. / SERIAL: R9 /ACCESSORIES: NONE / NOTE: A good entry level for Rolex Sub,  come with a plexiglass dome crystal as a vintage, with R serial the last batch of 5513.',
         carouselImgList: carouselConfig('1998 vintage rolex submariner 5513', 1, 7)
-    }, 
+    },
     {
         productIm: './prodImages/test2.png',
         title: '1988 Vintage Rolex Submariner 5513 Glossy Dial',
