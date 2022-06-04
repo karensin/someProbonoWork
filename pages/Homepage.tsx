7bff6e472848d8be843c486121dc07fa13bd92be
karensin <kkwsin@gmail.com>
2022-06-04 11:21:22 -0700

    testing

--- pages/Homepage.tsx
+++ pages/Homepage.tsx
@@ -80,7 +80,6 @@ const Homepage = () => {
 
     return (
         <div style={{ overflow: 'hidden' }} ref={dropdownRef}>
-
             <Row className={styles.homepage}>
                 <Col md={12} className="px-0" >
                     <Image alt="cover" className={styles.coverImg} src='/home_cover.png' />
