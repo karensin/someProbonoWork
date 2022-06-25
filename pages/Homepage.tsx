3011953b7a0f93359d21719400515d066bb97e89
karensin <kkwsin@gmail.com>
2022-06-25 12:56:20 -0700

    removed picture

--- pages/Homepage.tsx
+++ pages/Homepage.tsx
@@ -80,12 +80,8 @@ const Homepage = () => {
 
     return (
         <div style={{ overflow: 'hidden' }} ref={dropdownRef}>
-            <Row className={styles.homepage}>
-                <Col md={12} className={styles.test} >
-                </Col>
-            </Row>
             <div className={styles.productSection}>
-                <Row className="m-0 ">
+                <Row className="px-3">
                     {productsMock && productsMock.map((product: ProductProps, i) => {
                         return <Col className="p-2 my-2 mt-0 pt-0" md={4} key={i} > <Product productProps={product} /> </Col>
                     })}
